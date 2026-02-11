c4ae1c7ba7cffe653515235174ba24a47ba13718
Rocky7842 <eric.rocky7842@gmail.com>
2026-02-11 23:22:11 +0800

    platina: Convert to python extract utils
    Change-Id: Ia7f94a8704aff3c3a162b9567e94a9c2618689f7

---
+++ extract-files.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2026 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/xiaomi/sdm660-common',
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom/wlan/legacy',
+    'hardware/qcom-caf/msm8996',
+    'hardware/xiaomi',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libmmcamera_interface',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/libMiCameraHal.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+    'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
+    'vendor/lib/libVDSuperPhotoAPI.so': blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open'),
+    (
+        'vendor/lib64/hw/fingerprint.fpc.default.so',
+        'vendor/lib64/hw/fingerprint.goodix.default.so'
+    ): blob_fixup()
+        .fix_soname(),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'platina',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm660-common', module.vendor)
+    utils.run()
